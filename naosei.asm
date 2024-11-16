26d802f2db798782eaa69b516370a1feb26c7dc4
K(M)² <131779342+Kaemiemi@users.noreply.github.com>
2024-11-16 17:14:12 -0300

    100% FUNCIONAL  parte3(1).asm
    Método de colocar tudo em uma unica string e fazer a escrita de uma vez (problema de uma string absurdamente grande? sim, mas funciona...)

--- naosei.asm
+++ naosei.asm
@@ -1,10 +1,10 @@
 .data
-    vetor: .double 123.45, 10.2
-    TAM: .word 2
-    filename: .asciiz "output2.txt"
+    vetor: .double 123.45, 10.2, 1000.234, 666.66666
+    TAM: .word 4
+    filename: .asciiz "output.txt"
     string: .space 20          # Buffer para armazenar o float convertido em string
-    string2: .space 20 #string desenvertida
-    const_100000: .double 100000.0
+    string2: .space 100000 #string desenvertida
+    const_100: .double 100.0
 
 .text
 .globl main
@@ -22,10 +22,11 @@ main:
     la $t1, TAM
     lw $t1, 0($t1)
     li $t2, 0                    # Índice do vetor
+    
+    la $a2, string2
 
 loop_vetor:
 
-    
     l.d $f12, 0($s0)             # Carrega o valor do vetor em $f12
 
     # Converte a parte inteira do float
@@ -33,7 +34,7 @@ loop_vetor:
     mfc1 $a1, $f0
     la $a0, string
     
-    li $t6, 0
+#    li $t6, 0
     
 int_to_string:
     li $t3, 35
@@ -52,7 +53,7 @@ int_to_string_loop:
     
     bnez $a1, int_to_string_loop #continua o loop enquanto o quociente for !=0
     
-    la $a2, string2
+#    la $a2, string2
     j inverte_string
 
 coloca_ponto:
@@ -61,13 +62,12 @@ coloca_ponto:
     addi $t6, $t6, 1
     addi $a2, $a2, 1
     
- #   j escreve_arquivo
     
 parte_fracionada:
     cvt.d.w $f0, $f0 #converte uma word para double, nao sei se é realmente necessário...
     sub.d $f2, $f12, $f0 #diminui a parte inteira e deixa só a parte fracionaria
 
-    la $t7, const_100000
+    la $t7, const_100
     l.d $f10, 0($t7) #carrega o numero 100000.0 para a multiplicacao
     
     mul.d $f2, $f2, $f10 #multiplica
@@ -75,11 +75,9 @@ parte_fracionada:
     trunc.w.d $f2, $f2
     mfc1 $a1, $f2
     
-#    add $a0, $a0, $t5
     li $t3, 42
     sb $t3, 0($a0) #coloca * no comeco da string para declarar que é o comeco da parte fracionaria
-#    li $t5, 0
-#    addi $t5, $t5, 1
+
     addi $a0, $a0, 1
     
     loop_fracionada:
@@ -90,7 +88,7 @@ parte_fracionada:
     
     addi $t3, $t3, 48 #ascii
     sb $t3, 0($a0)
-#    addi $t5, $t5, 1 #numero de caracteres
+
     addi $a0, $a0, 1
     
     bnez $a1, loop_fracionada #continua o loop enquanto o quociente for !=0
@@ -102,7 +100,7 @@ inverte_string:
     lb $t4, 0($a0)
     
     beq $t4, 35, coloca_ponto #se o byte carregado for #, coloca ponto
-    beq $t4, 42, escreve_arquivo #se for *, finaliza
+    beq $t4, 42, proximo #se for *, finaliza
     
     sb $t4, 0($a2)
     addi $t6, $t6, 1
@@ -110,12 +108,20 @@ inverte_string:
     
     j inverte_string
     
+ proximo:
  
-escreve_arquivo:
-#    subi $t5, $t5, 1  # Reduz o tamanho para não incluir `#`
-    li $t3, 10
+     li $t3, 10 #adiciona \n
     sb $t3, 0($a2)
     addi $t6, $t6, 1
+    addi $a2, $a2, 1
+    
+#avança para o proximo valor do vetor
+addi $t2, $t2, 1
+addi $s0, $s0, 8
+bne $t2, $t1, loop_vetor        # Verifica se atingiu o final do vetor
+
+escreve_arquivo:
+
     
     
         # Syscall para escrever no arquivo
@@ -127,10 +133,6 @@ escreve_arquivo:
     move $t0, $v0
     
 
-#avança para o proximo valor do vetor
-addi $t2, $t2, 1
-addi $s0, $s0, 8
-bne $t2, $t1, loop_vetor        # Verifica se atingiu o final do vetor
 
 fimloop:
     # Fecha o arquivo
